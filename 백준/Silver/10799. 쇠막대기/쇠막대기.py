3afed90e03dc6cfeee01574a6a5dc4b2e326044e
SUNGWHANYOON <35682200+SUNGWHANYOON@users.noreply.github.com>
2024-07-11 20:59:03 +0900

    [Silver II] Title: 쇠막대기, Time: 80 ms, Memory: 36432 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+arr = list(input().rstrip())
+n = len(arr)
+
+lazer = 0
+stick = []
+stack = []
+
+for i in range(1,n,1):
+    if arr[i-1] =='(':
+        lazer+=1
+    if arr[i-1] == '(' and arr[i] ==')':
+        lazer-=1
+
+answer = lazer
+
+for i in range(n):
+    if arr[i] == '(':
+        stack.append((i,arr[i]))
+        continue
+
+    index, con = stack.pop()
+    if index + 1 == i:
+        answer += len(stack)
+
+print(answer)
