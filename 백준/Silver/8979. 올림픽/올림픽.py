9da83bd20ed951d7ae813eafdfe23fd95c35ecc6
SUNGWHANYOON <35682200+SUNGWHANYOON@users.noreply.github.com>
2024-08-06 14:18:56 +0900

    [Silver V] Title: 올림픽, Time: 64 ms, Memory: 34000 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -5,21 +5,21 @@ input = sys.stdin.readline
 n,k = map(int,input().split())
 
 arr = []
+find_nation_data = 0
 
 for i in range(n):
     score = deque(list(map(int,input().split())))
     nation = score.popleft()
-    score.append(nation)
+
+    if nation == k:
+        find_nation_data = list(score)
     arr.append(list(score))
 
 arr = sorted(arr,reverse=True)
-
 answer = 1
+for rank in arr:
+    if rank == find_nation_data:
+        break
+    answer+=1
 
-for i in range(1,n):
-    if arr[i][0:3] != arr[i-1][0:3]:
-        answer+=1
-
-    if arr[i][3] == k:
-        print(answer)
-        exit(0)
+print(answer)
